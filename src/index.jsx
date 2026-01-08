3182e513927e9357047447cb841f0021401fba39
Denis Libin <dnslibin4@gmail.com>
2026-01-08 22:39:48 +0200

    Create 'new products' gallery section

--- src/index.jsx
+++ src/index.jsx
@@ -11,7 +11,7 @@ import { RootCmp } from './RootCmp'
 
 import './assets/styles/main.scss'
 import "slick-carousel/slick/slick.css"
-import "slick-carousel/slick/slick-theme.css"
+// import "slick-carousel/slick/slick-theme.css"
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -8,6 +8,20 @@ export function HomePage() {
                 {/* <PauseOnHover /> */}
                 <EmblaCarousel />
             </div>
+            <div className="section-separator">
+                <h2>חדש על המדף</h2>
+            </div>
+            <div className="gallery2">
+                <img src="https://zolstock.co.il/wp-content/uploads/2025/10/456456454565.jpg" alt="new-products1" />
+                <img src="https://zolstock.co.il/wp-content/uploads/2025/10/575676567.jpg" alt="new-products2" />
+                <img src="https://zolstock.co.il/wp-content/uploads/2025/10/9909090.jpg" alt="new-products3" />
+                <img src="https://zolstock.co.il/wp-content/uploads/2025/10/23423424.jpg" alt="new-products4" />
+                <img src="https://zolstock.co.il/wp-content/uploads/2025/10/23321323123.jpg" alt="new-products5" />
+                <img src="https://zolstock.co.il/wp-content/uploads/2025/10/788989.jpg" alt="new-products6" />
+            </div>
+            <div className="all-products-btn">
+                <button>לכל המוצרים</button>
+            </div>
             <div className="welcome">
                 <h1>ברוכים הבאים לרשת זול סטוק!</h1>
                 <p>רשת זול סטוק מציעה חוויית קנייה משתלמת עם מגוון עצום של מוצרי צריכה לבית ולמשפחה במחירים זולים במיוחד. ברשת תוכלו למצוא צעצועים, כלי בית, טקסטיל, מוצרי פארם וניקיון, ציוד משרדי, מוצרי חשמל ועוד – הכול מתחדש באופן קבוע. בזול סטוק שמים את הלקוח והמחיר במרכז, עם שירות מקצועי והתאמה לכל תקציב. <strong>“זול סטוק – כשמחיר וחוויה נפגשים”</strong></p>
